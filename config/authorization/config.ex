56773f0e616aaae92e44225cb45f6aacc4680549
Michaël Dierick <michael.dierick@redpencil.io>
2019-11-08 21:27:19 +0100

    Added temporary mu-auth config

---
+++ config/authorization/config.ex
@@ -0,0 +1,46 @@
+alias Acl.Accessibility.Always, as: AlwaysAccessible
+alias Acl.Accessibility.ByQuery, as: AccessByQuery
+alias Acl.GraphSpec.Constraint.Resource.AllPredicates, as: AllPredicates
+alias Acl.GraphSpec.Constraint.Resource.NoPredicates, as: NoPredicates
+alias Acl.GraphSpec.Constraint.ResourceFormat, as: ResourceFormatConstraint
+alias Acl.GraphSpec.Constraint.Resource, as: ResourceConstraint
+alias Acl.GraphSpec, as: GraphSpec
+alias Acl.GroupSpec, as: GroupSpec
+alias Acl.GroupSpec.GraphCleanup, as: GraphCleanup
+
+defmodule Acl.UserGroups.Config do
+  def user_groups do
+    [
+      # // PUBLIC
+      %GroupSpec{
+        name: "public",
+        useage: [:read,:write,:read_for_write],
+        access: %AlwaysAccessible{}, # TODO: Should be only for logged in users
+        graphs: [ 
+                  %GraphSpec{
+                    graph: "http://mu.semte.ch/graphs/public",
+                    constraint: %ResourceConstraint{
+                      resource_types: [
+                        "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#FileDataObject",
+                        "http://data.vlaanderen.be/ns/besluit#Bestuursorgaan",
+                        "http://mu.semte.ch/vocabularies/ext/oc/Meeting",
+                        "http://mu.semte.ch/vocabularies/ext/oc/AgendaItem",
+                        "http://mu.semte.ch/vocabularies/ext/oc/Case",
+                        "http://xmlns.com/foaf/0.1/Person",
+                        "http://xmlns.com/foaf/0.1/OnlineAccount",
+                        "http://xmlns.com/foaf/0.1/Group"
+                      ]
+                    }
+                  }
+                ]
+      },
+      # // CLEANUP
+      #
+      %GraphCleanup{
+        originating_graph: "http://mu.semte.ch/application",
+        useage: [:write],
+        name: "clean"
+      }
+    ]
+  end
+end
