0f8289259bbd9cef37e0e548cbc7977e4c30bdfd
Michaël Dierick <michael.dierick@redpencil.io>
2020-01-06 11:55:07 +0100

    Wire delta to resource

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -14,5 +14,21 @@ export default [
       gracePeriod: 10000,
       ignoreFromSelf: true
     }
+  },
+  {
+    match: {
+      // form of element is {subject, predicate, object}
+      subject: {
+      }
+    },
+    callback: {
+      url: 'http://resource/.mu/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 250,
+      ignoreFromSelf: true
+    }
   }
 ];
