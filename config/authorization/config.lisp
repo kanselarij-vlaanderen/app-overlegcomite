add03b905a3366d4ac1164946c7a6002e3981a9c
Erika Pauwels <erika.pauwels@gmail.com>
2026-07-01 14:17:07 +0200

    Remove all user related configuration and services
    We're no longer gonna manage users and their groups in the app. They
    will be managed by ACM/IDM

--- config/authorization/config.lisp
+++ config/authorization/config.lisp
@@ -27,7 +27,7 @@
   :nfo "http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#"
   :ext "http://mu.semte.ch/vocabularies/ext/")
 
-(defun access-by-role-query (group-uris)
+(defun access-by-role (group-uris)
   (format nil "PREFIX session: <http://mu.semte.ch/vocabularies/session/>
               PREFIX foaf: <http://xmlns.com/foaf/0.1/>
               SELECT ?group_uri WHERE {
@@ -39,66 +39,41 @@
 (supply-allowed-group "public")
 
 (supply-allowed-group "admin-rw"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/admin>"))
+  :query (access-by-role "<http://data.kanselarij.vlaanderen.be/id/group/admin>"))
+
 (supply-allowed-group "kanselarij"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/kanselarij>"))
+  :query (access-by-role "<http://data.kanselarij.vlaanderen.be/id/group/kanselarij>"))
+
 (supply-allowed-group "minister"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/minister>"))
-(supply-allowed-group "kabinet"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/kabinet>"))
-(supply-allowed-group "adviesverlener"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/adviesverlener>"))
+  :query (access-by-role "<http://data.kanselarij.vlaanderen.be/id/group/minister>"))
+
 (supply-allowed-group "kabinet-adviesverlener"
-  :query (access-by-role-query
+  :query (access-by-role
     "<http://data.kanselarij.vlaanderen.be/id/group/kabinet>
     <http://data.kanselarij.vlaanderen.be/id/group/adviesverlener>"))
-(supply-allowed-group "administratie"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/administratie>"))
-(supply-allowed-group "parlement"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/parlement>"))
+
 (supply-allowed-group "administratie-parlement"
-  :query (access-by-role-query
+  :query (access-by-role
     "<http://data.kanselarij.vlaanderen.be/id/group/administratie>
     <http://data.kanselarij.vlaanderen.be/id/group/parlement>"))
-(supply-allowed-group "user"
-  :query (access-by-role-query "<http://data.kanselarij.vlaanderen.be/id/group/user>"))
 
 (define-graph public ("http://mu.semte.ch/graphs/public")
   ;; Fixed code list types
   ("ext:DocumentTypeCode" -> _)
   ("ext:ToegangsniveauCode" -> _)
   ("besluit:Bestuursorgaan" -> _)
-  ("foaf:Group" -> _)
   ("org:Organization" -> _)
   ;; Account info types
   ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
-
-(define-graph admin-user-info ("http://mu.semte.ch/graphs/account-info")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
-
-(define-graph authenticated-users-case ("http://mu.semte.ch/graphs/authenticated-users")
-    ("oc:Case" -> _))
+  ("foaf:OnlineAccount" -> _))
 
 (define-graph kanselarij-agenda ("http://mu.semte.ch/graphs/organizations/kanselarij")
+  ("oc:Case" -> _)
   ("oc:Meeting" -> _)
   ("oc:AgendaItem" -> _)
   ("foaf:Document" -> _)
   ("ext:DocumentVersie" -> _)
   ("nfo:FileDataObject" -> _))
-(define-graph kanselarij-account-info ("http://mu.semte.ch/graphs/organizations/kanselarij")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
 
 (define-graph minister-agenda ("http://mu.semte.ch/graphs/organizations/minister")
   ("oc:Meeting" -> _)
@@ -106,12 +81,6 @@
   ("foaf:Document" -> _)
   ("ext:DocumentVersie" -> _)
   ("nfo:FileDataObject" -> _))
-(define-graph minister-account-info ("http://mu.semte.ch/graphs/organizations/minister")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
 
 (define-graph kabinet-agenda ("http://mu.semte.ch/graphs/organizations/kabinet")
   ("oc:Meeting" -> _)
@@ -119,19 +88,6 @@
   ("foaf:Document" -> _)
   ("ext:DocumentVersie" -> _)
   ("nfo:FileDataObject" -> _))
-(define-graph kabinet-account-info ("http://mu.semte.ch/graphs/organizations/kabinet")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
-
-(define-graph adviesverlener-account-info ("http://mu.semte.ch/graphs/organizations/adviesverlener")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
 
 (define-graph administratie-agenda ("http://mu.semte.ch/graphs/organizations/administratie")
   ("oc:Meeting" -> _)
@@ -140,85 +96,26 @@
   ("ext:DocumentVersie" -> _)
   ("nfo:FileDataObject" -> _))
 
-(define-graph administratie-account-info ("http://mu.semte.ch/graphs/organizations/administratie")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
-
-(define-graph parlement-account-info ("http://mu.semte.ch/graphs/organizations/parlement")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
-
-(define-graph user-account-info ("http://mu.semte.ch/graphs/organizations/user")
-  ("foaf:Person" -> _)
-  ("foaf:OnlineAccount" -> _)
-  ("adms:Identifier" -> _)
-  ;; ideally only writes on "http://xmlns.com/foaf/0.1/member" predicate
-  ("foaf:Group" -> _))
-
 (grant (read)
   :to-graph public
   :for-allowed-group "public")
 
 (grant (read write)
-  :to-graph (
-    admin-user-info
-    authenticated-users-case
-    kanselarij-agenda)
+  :to-graph (kanselarij-agenda)
   :for-allowed-group "admin-rw")
 
 (grant (read write)
-  :to-graph (
-    kanselarij-agenda
-    kanselarij-account-info
-    authenticated-users-case)
+  :to-graph (kanselarij-agenda)
   :for-allowed-group "kanselarij")
 
 (grant (read)
-  :to-graph (
-    minister-agenda
-    minister-account-info
-    authenticated-users-case)
+  :to-graph (minister-agenda)
   :for-allowed-group "minister")
 
 (grant (read)
-  :to-graph (
-    kabinet-agenda
-    authenticated-users-case)
+  :to-graph (kabinet-agenda)
   :for-allowed-group "kabinet-adviesverlener")
 
 (grant (read)
-  :to-graph (
-    kabinet-account-info)
-  :for-allowed-group "kabinet")
-
-(grant (read)
-  :to-graph (
-    adviesverlener-account-info)
-  :for-allowed-group "adviesverlener")
-
-(grant (read)
-  :to-graph (
-    administratie-agenda
-    authenticated-users-case)
+  :to-graph (administratie-agenda)
   :for-allowed-group "administratie-parlement")
-
-(grant (read)
-  :to-graph (
-    administratie-account-info)
-  :for-allowed-group "administratie")
-
-(grant (read)
-  :to-graph (
-    parlement-account-info)
-  :for-allowed-group "parlement")
-
-(grant (read)
-  :to-graph (
-    user-account-info)
-  :for-allowed-group "user")
