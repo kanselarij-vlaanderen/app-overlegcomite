5e0a6f5dedac2caeb470ef75275e0fb392b57396
Erika Pauwels <erika.pauwels@gmail.com>
2026-08-08 12:35:41 +0200

    Remove outdated delta rules of removed services

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -33,22 +33,6 @@ export default [
   },
   {
     match: {
-      // form of element is {subject, predicate, object}
-      subject: {
-      }
-    },
-    callback: {
-      url: 'http://user-info-distribution/delta',
-      method: 'POST'
-    },
-    options: {
-      resourceFormat: 'v0.0.1',
-      gracePeriod: 5000,
-      ignoreFromSelf: true
-    }
-  },
-  {
-    match: {
       predicate: { type: 'uri', value: 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type' },
       object: { type: 'uri', value: 'http://mu.semte.ch/vocabularies/ext/oc/Meeting' }
     },
